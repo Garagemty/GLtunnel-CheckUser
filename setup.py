6cba4b350ff43d0e8822ef7adcbcd70847e5e7cf
osvaldo <osvaldoguerreiro53@gmail.com>
2023-01-06 06:51:56 -0300

    osvaldo

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ VERSION = __version__
 DESCRIPTION = open('README.md').read()
 AUTHOR = __author__
 AUTHOR_EMAIL = __email__
-URL = 'https://github.com/DuTra01/CheckUser'
+URL = 'https://github.com/osvaldo/gl-mold/CheckUser'
 LICENSE = 'MIT'
 
 setup(
